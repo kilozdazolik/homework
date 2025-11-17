97dd4c7c58267e4ff1ea7706de1ee2962182898d
kilozdazolik <paulsonzoltan@protonmail.com>
2025-11-17 12:04:47 +0100

    feat: create Policy model for ztpspf

---
+++ src/main/java/com/nn/homework/Models/Policy.java
@@ -0,0 +1,38 @@
+package com.nn.homework.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "dbo.Policy")
+@Data
+public class Policy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Chdrnum", nullable = false, length = 8)
+    private String chdrnum;
+
+    @Column(name = "Cownnum", nullable = false, length = 8)
+    private String cownnum;
+
+    @Column(name = "OwnerName", length = 50)
+    private String ownerName;
+
+    @Column(name = "LifcNum", length = 8)
+    private String lifcNum;
+
+    @Column(name = "LifcName", length = 50)
+    private String lifchName;
+
+    @Column(name = "Aracde", length = 3)
+    private String aracde;
+
+    @Column(name = "Agntnum", length = 5)
+    private String agntnum;
+
+    @Column(name = "MailAddress", length = 50)
+    private String mailAddress;
+
+}
